3a97127bac5d351446b1f4e3439322d30012e305
JP Robinson <john.paul.robinson@gmail.com>
2018-01-03 19:05:20 -0500

    using real struct from realworld

--- df.go
+++ df.go
@@ -10,60 +10,99 @@ type FulfillmentResponse struct {
 }
 
 type FulfillmentRequest struct {
-	Lang   string `json:"lang"`
-	Status struct {
-		ErrorType string `json:"errorType"`
-		Code      int    `json:"code"`
-	} `json:"status"`
-	Timestamp time.Time `json:"timestamp"`
-	SessionID string    `json:"sessionId"`
-	Result    struct {
-		Parameters    map[string]interface{} `json:"parameters"`
-		Contexts      []interface{}          `json:"contexts"`
-		ResolvedQuery string                 `json:"resolvedQuery"`
-		Source        string                 `json:"source"`
-		Score         float64                `json:"score"`
-		Speech        string                 `json:"speech"`
-		Fulfillment   struct {
-			Messages []struct {
-				Speech string      `json:"speech"`
-				Type   interface{} `json:"type"`
-			} `json:"messages"`
-			Speech string `json:"speech"`
-		} `json:"fulfillment"`
-		ActionIncomplete bool   `json:"actionIncomplete"`
-		Action           string `json:"action"`
-		Metadata         struct {
-			IntentID                  string `json:"intentId"`
-			WebhookForSlotFillingUsed string `json:"webhookForSlotFillingUsed"`
-			IntentName                string `json:"intentName"`
-			WebhookUsed               string `json:"webhookUsed"`
-		} `json:"metadata"`
-	} `json:"result"`
-	ID              string `json:"id"`
 	OriginalRequest struct {
-		Source string `json:"source"`
-		Data   struct {
+		Source  string `json:"source"`
+		Version string `json:"version"`
+		Data    struct {
+			IsInSandbox bool `json:"isInSandbox"`
+			Surface     struct {
+				Capabilities []struct {
+					Name string `json:"name"`
+				} `json:"capabilities"`
+			} `json:"surface"`
 			Inputs []struct {
 				RawInputs []struct {
-					Query     string      `json:"query"`
-					InputType interface{} `json:"input_type"`
-				} `json:"raw_inputs"`
-				Intent    string `json:"intent"`
+					Query     string `json:"query"`
+					InputType string `json:"inputType"`
+				} `json:"rawInputs"`
 				Arguments []struct {
-					TextValue string `json:"text_value"`
-					RawText   string `json:"raw_text"`
+					RawText   string `json:"rawText"`
+					TextValue string `json:"textValue"`
 					Name      string `json:"name"`
 				} `json:"arguments"`
+				Intent string `json:"intent"`
 			} `json:"inputs"`
 			User struct {
-				UserID string `json:"user_id"`
+				LastSeen time.Time `json:"lastSeen"`
+				Locale   string    `json:"locale"`
+				UserID   string    `json:"userId"`
 			} `json:"user"`
 			Conversation struct {
-				ConversationID    string      `json:"conversation_id"`
-				Type              interface{} `json:"type"`
-				ConversationToken string      `json:"conversation_token"`
+				ConversationID    string `json:"conversationId"`
+				Type              string `json:"type"`
+				ConversationToken string `json:"conversationToken"`
 			} `json:"conversation"`
+			AvailableSurfaces []struct {
+				Capabilities []struct {
+					Name string `json:"name"`
+				} `json:"capabilities"`
+			} `json:"availableSurfaces"`
 		} `json:"data"`
 	} `json:"originalRequest"`
+	ID        string    `json:"id"`
+	Timestamp time.Time `json:"timestamp"`
+	Lang      string    `json:"lang"`
+	Result    struct {
+		Source           string `json:"source"`
+		ResolvedQuery    string `json:"resolvedQuery"`
+		Speech           string `json:"speech"`
+		Action           string `json:"action"`
+		ActionIncomplete bool   `json:"actionIncomplete"`
+		Parameters       struct {
+			SubwayStop      string `json:"subway-stop"`
+			SubwayLine      string `json:"subway-line"`
+			SubwayDirection string `json:"subway-direction"`
+		} `json:"parameters"`
+		Contexts []struct {
+			Name       string `json:"name"`
+			Parameters struct {
+				SubwayStop              string `json:"subway-stop"`
+				SubwayStopOriginal      string `json:"subway-stop.original"`
+				SubwayDirectionOriginal string `json:"subway-direction.original"`
+				SubwayLineOriginal      string `json:"subway-line.original"`
+				SubwayLine              string `json:"subway-line"`
+				SubwayDirection         string `json:"subway-direction"`
+			} `json:"parameters"`
+			Lifespan int `json:"lifespan"`
+		} `json:"contexts"`
+		Metadata struct {
+			MatchedParameters []struct {
+				Required bool   `json:"required"`
+				DataType string `json:"dataType"`
+				Name     string `json:"name"`
+				Value    string `json:"value"`
+				Prompts  []struct {
+					Lang  string `json:"lang"`
+					Value string `json:"value"`
+				} `json:"prompts"`
+				IsList bool `json:"isList"`
+			} `json:"matchedParameters"`
+			IntentName                string `json:"intentName"`
+			IntentID                  string `json:"intentId"`
+			WebhookUsed               string `json:"webhookUsed"`
+			WebhookForSlotFillingUsed string `json:"webhookForSlotFillingUsed"`
+			NluResponseTime           int    `json:"nluResponseTime"`
+		} `json:"metadata"`
+		Fulfillment struct {
+			Speech   string        `json:"speech"`
+			Messages []interface{} `json:"messages"`
+		} `json:"fulfillment"`
+		Score float64 `json:"score"`
+	} `json:"result"`
+	Status struct {
+		Code            int    `json:"code"`
+		ErrorType       string `json:"errorType"`
+		WebhookTimedOut bool   `json:"webhookTimedOut"`
+	} `json:"status"`
+	SessionID string `json:"sessionId"`
 }
