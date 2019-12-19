2a75342ddbfa35ab5c9782e876bd025b20317d15
JP Robinson <john.paul.robinson@gmail.com>
2019-12-19 14:44:21 -0500

    removing log

--- dialogflow.go
+++ dialogflow.go
@@ -70,7 +70,6 @@ func (s *service) decode(ctx context.Context, r *http.Request) (interface{}, err
 
 	req.OriginalDetectIntentRequest.Payload.User.UserID = claims.Sub
 	req.OriginalDetectIntentRequest.Payload.User.Email = claims.Email
-	kit.Logger(ctx).Log("requuest?", req)
 	return &req, nil
 }
 
